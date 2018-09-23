d312835bd80f82077e78b634fcce78662b9854ce
Victor Rivera <verm2405@gmail.com>
2018-09-23 16:59:20 -0400

    Finished basic movie site. Included styled components and Overdrive. Will build upon for practice later

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import Movie from "./Movie";
+import MoviesList from "./MoviesList";
+import MovieDetail from "./MovieDetail";
 
-const movies = [
-  {
-    id: 1,
-    title: "Star Wars"
-  },
-  {
-    id: 2,
-    title: "Avengers Infinity War"
-  },
-  {
-    id: 3,
-    title: "Wonder Woman"
-  },
-  {
-    id: 4,
-    title: "Baby Driver"
-  }
-];
-
-class App extends Component {
-  state = {
-    movies: []
-  };
-  async componentDidMount() {
-    try {
-      const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=a5326823e52c473ffda44ace64b7d44d&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
-      );
-      const movies = await res.json();
-      this.setState({
-        movies: movies.results
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Link to="/">
           <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        {this.state.movies.map(movie => (
-          <Movie key={movie.id} movie={movie} />
-        ))}
-      </div>
-    );
-  }
-}
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MoviesList} />
+        <Route path="/:id" component={MovieDetail} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default App;
+
+const Test = ({ match }) => <h1>{match.params.id}</h1>;
--- src/Movie.js
+++ src/Movie.js
@@ -1,17 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import Overdrive from "react-overdrive";
 
-class Movie extends Component {
-  render() {
-    //const { title } = this.props.movie;
-    const { title } = this.props.movie;
-    return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-        {/* <p>{this.props.desc}</p> */}
-      </div>
-    );
-  }
-}
+const POSTER_PATH = "http://image.tmdb.org/t/p/w154";
+
+const Movie = ({ movie }) => (
+  <Link to={`/${movie.id}`}>
+    <Overdrive id={movie.id}>
+      <Poster src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+    </Overdrive>
+  </Link>
+);
 
 export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired
+  }).isRequired
+};
+
+export const Poster = styled.img`
+  box-shadow: 0 0 35px black;
+`;
